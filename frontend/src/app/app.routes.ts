35d6ad85d60a35999359f26d2e0430968867e06b
kauanvinicius9 <kauan.francischini@aluno.senai.br>
2026-07-23 19:57:09 -0300

    add new page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ProductsComponent } from './features/products/products.component';
 import { CartComponent } from './features/cart/cart.component';
 import { CheckoutComponent } from './features/checkout/checkout.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { AboutProductsComponent } from './features/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Reviva | Velas & Aromas' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'carrinho', component: CartComponent, title: 'Carrinho | Reviva' },
   { path: 'checkout', component: CheckoutComponent, title: 'Finalizar Compra | Reviva' },
   { path: 'contato', component: ContactComponent, title: 'Contato | Reviva' },
+  { path: 'sobre', component: AboutProductsComponent, title: 'Sobre | Reviva' },
   { path: '**', redirectTo: '' }
 ];
