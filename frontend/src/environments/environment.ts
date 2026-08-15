5fea954166ec87f5b0dcb717e25f0d4158138647
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-08-14 23:14:59 -0300

    Excluir environment.ts

--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://revivavelasbackend.onrender.com/api',
-  mercadoPagoPublicKey: "APP_USR-bbb339c8-117f-4d9f-884e-5b0657bd731f"
-};
